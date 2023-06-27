c68020888abce0a9b17a57cb4abba182fdacdddd
Yana Tolstobrova <jana.tolstobrova@gmail.com>
2023-06-27 22:29:55 +0200

    cards component con api call

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
-
+import Main from './pages/Main'
 import Intro from './pages/Intro.jsx';
 
 const App = () => {
   return (
-    
+      <div>
       <Intro />
+      <Main/>
+      </div>
+      
+      
     
   );
 };

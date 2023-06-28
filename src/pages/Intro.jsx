1fe97498c8565a6ee094e902ce4baef60a545b6f
dkhurlop <denisekhurlopian@gmail.com>
2023-06-28 12:58:33 +0200

    eliminación de carpetas vacias

--- src/pages/Intro.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Button from "../components/button/Button";
-
-export default function Intro() {
-  return (
-      <Button />
-  )
-}
---
+++ src/pages/Intro/Intro.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Button from "../../components/Button/Button";
+
+export default function Intro() {
+  return (
+      <Button />
+  )
+}

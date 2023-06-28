ecf4eaf44c522b00a2145485cab18bfde6ea2537
Yana Tolstobrova <jana.tolstobrova@gmail.com>
2023-06-28 10:14:53 +0200

    global styles, routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-
-import Main from './pages/Main'
 import Intro from './pages/Intro.jsx';
 
 const App = () => {
   return (
       <div>
       <Intro />
-      <Main/>
       </div>
       
       

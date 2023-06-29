277a7adeb3d331c02d926d1cc77a97dfd240e2c9
genesis-nf <genefernandz@gmail.com>
2023-06-29 09:54:07 +0200

    Header modificado css y Link de regreso

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import Header from '../components/Header/Header'
+import React from 'react';
+import Header from '../components/Header/Header';
+import AstralReadings from '../components/AstralReadings/AstralReadings';
 
 export default function Favorites() {
   return (
     <div>
       <Header/>
-      
+      <AstralReadings/>
     </div>
   )
 }

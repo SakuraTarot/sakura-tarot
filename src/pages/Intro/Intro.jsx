9cdff1e95188c6f7f6515af1a515e1c8833f559c
rosa-rubio <rosamrubio@gmail.com>
2023-06-29 10:29:57 +0200

    Creación de página Intro

--- src/pages/Intro/Intro.jsx
+++ src/pages/Intro/Intro.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import MainIntro from "../../components/MainIntro/MainIntro"
 import Button from "../../components/button/Button";
+import "./Intro.css";
 
 export default function Intro() {
   return (
+    <>
+      <div className="intro">
+      <MainIntro />
       <Button />
+      </div>
+    </>
   )
 }

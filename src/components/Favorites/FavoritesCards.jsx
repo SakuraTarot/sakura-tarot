578b7e8b997ee8fb4dff05d740b4bf35930adf46
dkhurlop <denisekhurlopian@gmail.com>
2023-07-05 12:18:02 +0200

    delete favoritecard

--- src/components/Favorites/FavoritesCards.jsx
+++ src/components/Favorites/FavoritesCards.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import './FavoritesCards.css';
+import { useState } from 'react';
+
 
 export default function FavoritesCards() {
-    const savedData = [];
+  const [savedData, setSavedData] = useState([]);
+    
   for (let i = 0; i < localStorage.length; i++) {
     const key = localStorage.key(i);
     if (key.startsWith('Saved_')) {
@@ -10,8 +13,14 @@ export default function FavoritesCards() {
       savedData.push(JSON.parse(data));
     }
   }
-      
-  const divArray = [];
+  const handleDelete = (index) => {
+    const keyToDelete = `Saved_${index}`;
+    localStorage.removeItem(keyToDelete);
+    savedData.splice(index, 1);
+    setSavedData([...savedData]);
+  };
+  
+      const divArray = [];
     for (let i = 0; i < localStorage.length; i++) {
         divArray.push(
           <div key={i} className='savedBox'>
@@ -20,7 +29,7 @@ export default function FavoritesCards() {
           <div className='line'><strong>Presente:</strong> {savedData[i].selectedCards[1].meaning}</div>
             <div><strong>Futuro:</strong> {savedData[i].selectedCards[2].meaning}</div>
             <div>{savedData[i].textareaValue}</div>
-            <textarea value={savedData[i].textareaValue}></textarea>
+            <button onClick={() => handleDelete(i)}>Eliminar</button>
       </div>
     );
     }

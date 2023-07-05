40647a00fa33b284af18a261cd40bf09a6328503
genesis-nf <genefernandz@gmail.com>
2023-07-05 10:58:53 +0200

    intento de unificar codigos

---
+++ src/components/FavoritesCards/FavoritesCards.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+
+export default function FavoritesCards() {
+    const divArray = [];
+    for (let i = 0; i < localStorage.length/2; i++){
+        const key1 = `selectedCards_${i}`;
+        const value = localStorage.getItem(key1);
+        const objectArray = JSON.parse(value);
+        const pasadoArray = [objectArray[0].spanishName, objectArray[0].meaning];
+        const presenteArray = [objectArray[1].spanishName, objectArray[1].meaning];
+        const futuroArray = [objectArray[2].spanishName, objectArray[2].meaning];
+        const key2 = `Date_${i}`;
+        const savedDate = localStorage.getItem(key2);
+        divArray.push(
+          <div key={key1} className="savedBox">
+            <div className="date">{savedDate}</div>
+          <div className="line"><strong>Pasado:</strong> {pasadoArray[0]}, {pasadoArray[1]}</div>
+          <div className="line"><strong>Presente:</strong> {presenteArray[0]}, {presenteArray[1]}</div>
+          <div><strong>Futuro:</strong> {futuroArray[0]}, {futuroArray[1]}</div>
+      </div>
+    );
+    }
+    const reversed = divArray.reverse()
+  return (
+    <div>
+        {reversed}
+    </div>
+  )
+}

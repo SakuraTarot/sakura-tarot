1ddfaaccb1ab2d2ebc5ab591f587f3a1caf11b58
Teresa1965 <132339878+Teresa1965@users.noreply.github.com>
2023-07-06 15:34:04 +0200

    test boton superado

--- src/components/button/Button.test.js
+++ src/components/button/Button.test.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Button from './Button';
+import { MemoryRouter } from "react-router-dom";
 
 describe('Button', () => {
   it('renders without errors', () => {
-    render(<Button />);
-    const buttonElement = screen.getByRole('button');
+    render(
+    <MemoryRouter>
+    <Button />
+  </MemoryRouter>);
+    const buttonElement = screen.getByText('Saber mi destino');
     expect(buttonElement).toBeInTheDocument();
   });
 });

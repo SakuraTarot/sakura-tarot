edc00c6945c6ef17524832f88de2392f2eb3dca5
Yana Tolstobrova <jana.tolstobrova@gmail.com>
2023-07-06 12:57:32 +0200

    Revert "Revert "Merge pull request #5 from SakuraTarot/modal-in-favorites-page""
    This reverts commit 561f0b736eff636db09c4e36eb3389e763dec406.

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header/Header';
 import FavoritesCards from '../components/Favorites/FavoritesCards';
+import Background from '../components/Background/Background';
 
 
 export default function Favorites() {
   return (
     <div>
+      <Background />
       <Header/>
       <FavoritesCards />
     </div>
